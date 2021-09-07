4b829ab9859da37807f98800182053ee031679d6
prasanai <prasannakumar.nair@ymail.com>
2021-09-07 11:58:15 +0530

    Default commit

---
+++ app/src/main/java/com/prasannakumar/dindinnassignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.prasannakumar.dindinnassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
